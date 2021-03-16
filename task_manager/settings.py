b2f8e2d27dec078ef05673a6952c1355d3913eef
devriez <a.moiseikin@gmail.com>
2021-03-16 18:27:22 +0300

    update requirements.txt

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,9 +32,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nr!!^hk09x*^9v85_$kmv*28ve=w8_xl4id4xrdkwsk+yv4c=)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
