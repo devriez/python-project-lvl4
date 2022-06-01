cc9e2cd0c36ea14b711163b17b1f9ab6a263fbbe
devriez <a.moiseikin@gmail.com>
2022-06-01 17:54:08 +0500

    change success url in login view

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 class UserLogin(LoginView):
     template_name = 'task_manager/login.html'
     success_message = 'Вы залогинены'
-    success_url = reverse_lazy('users-list')
+    success_url = reverse_lazy('start-page')
 
 
 class UserLogout(LogoutView):
