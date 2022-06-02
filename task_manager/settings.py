bd85ca0abaa0917bd052208dc95bad2ac0d4e748
devriez <a.moiseikin@gmail.com>
2022-06-02 12:34:00 +0500

    add success message to login and logout

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config()
 
 if db_from_env:
     DATABASES = {
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 
 
-class UserLogin(LoginView):
+class UserLogin(SuccessMessageMixin, LoginView):
     template_name = 'task_manager/login.html'
     success_message = 'Вы залогинены'
     success_url = reverse_lazy('start-page')
 
 
 class UserLogout(LogoutView):
-    success_message = 'Вы разлогинены'
-    success_url = reverse_lazy('users-list')
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, 'Вы разлогинены')
+        return super().dispatch(request, *args, **kwargs)
